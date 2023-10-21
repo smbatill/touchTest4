785033e0e5a37dcb6c4b80ac159dcf73f345894e
RetProf <batill@nd.edu>
2023-10-21 16:20:44 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,29 +16,26 @@ function draw() {
   fill(0,0,255);
   text('test',50,50);
 
+// mouseIsPressed stores the current mouse position on computer
+//  the computer touchpad operation is erratic not sure how it is set-up 
+
   if(mouseIsPressed === true){
     fill(255,0,0);
-    rect(mouseX,mouseY,20,20);
+    ellipse(mouseX,mouseY,100,100);
   }
 
 
   if(touches.length >= 0){
-    text(touches.length,100,100);
-//    tposX = touches.x[1];
-//     tposY = touches.y[1];
-//   rect(tposX,tposY,30,30);
+    text(touches.length,20,20);
     }   
-/*
-    fill(255,0,0);
-    rect(touches[0].x,touches[0].y,40,40);
-    for(let i = 0; i <= touches.length; i++){
-     fill(0);
-     text(touches.length,touches[i].x+20,touches[i].y+20);
-    }
-
-  }
-*/
 }
+
+// when a touch screen is touched touches objects are populated
+// the touches.length variable indicates the number of simultaneous touchs
+// it is "0" with no touches and returns to 0 when the touches are removed
+// not quite sure yet what happens to touches.x and touches.y when the
+// touches are removed but the touches object DOES NOT populate on a computer
+// i.e. no touch 
 
 function touchStarted() {
   if (value === 0) {
