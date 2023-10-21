8379b1eeca465bb41de84bc527706026ef23d549
RetProf <batill@nd.edu>
2023-10-21 10:04:35 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,14 +7,15 @@ function setup(){
 }
 
 function draw() {
+  textSize(10);
   fill(value);
   rect(175, 175, 50, 50);
 
   if(touches.length > 0)
     {fill(255,0,0);
     rect(touches[0].x,touches[0].y,40,40);
+    text(touches.length,touches[0].x,touches[0].y);
     }
-
 
 }
 
